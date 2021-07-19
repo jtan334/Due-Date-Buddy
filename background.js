4b65acb32e3c459c9f80b6fbf9be6c20cb633a20
jtan334 <jtan334@uwo.ca>
2021-07-19 16:43:26 -0400

    updated the button layout

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-let color = '	 #b3e6cc';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create({url: 'index.html'});
 });
